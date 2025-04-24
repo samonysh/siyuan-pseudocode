1bc4630da92f030352c60cd06be2f3adfd175057
Samon Yu <13961835623@163.com>
2025-04-24 09:40:50 +0800

    feat的功能(pseudocode): 增加显示块结束符的选项
    - 在 index.html 中添加了显示块结束符的复选框
    - 在 main.js 中实现了块结束符显示的逻辑
    - 更新了本地存储和 API 调用以保存用户设置

--- src/main.js
+++ src/main.js
@@ -20,8 +20,13 @@ try {
   // 输入Latex格式的代码
   const inputText = document.getElementById("input-text");
   // 按钮
+  // 显示行号
   const lineNumber = document.getElementById("line-number");
   const lineNumberContainer = document.getElementById("line-number-container");
+  // 显示块结束符
+  const blockEnding = document.getElementById("block-ending");
+  const blockEndingContainer = document.getElementById("block-ending-container");
+  // 提交与修改
   const submit = document.getElementById("submit");
   const modify = document.getElementById("modify");
   // 输出
@@ -35,6 +40,9 @@ try {
       // 生成 pre 标签，设置内容
       let outputCode = document.createElement("pre");
       outputCode.textContent = savedContent.data["custom-latex-code"];
+      lineNumber.checked = JSON.parse(savedContent.data["custom-line-number"]);
+      blockEnding.checked = JSON.parse(savedContent.data["custom-block-ending"]);
+
       outputCode.className = "pseudocode";
       outputCode.id = "output-code";
       outputCode.style.display = "block";
@@ -44,10 +52,12 @@ try {
       inputText.style.display = "none";
       modify.style.display = "block";
       lineNumberContainer.style.display = "none";
+      blockEndingContainer.style.display = "none";
       submit.style.display = "none";
       // 渲染伪代码
       pseudocode.renderElement(document.getElementById("output-code"), {
         lineNumber: lineNumber.checked,
+        noEnd: !blockEnding.checked,
       });
     }
   };
@@ -62,9 +72,21 @@ try {
     // 获取输入的Latex格式的代码
     const inputTextValue = inputText.value;
     // 将输入的代码保存到本地存储中
-    let response = await addBlockAttrAPI(
+    let codeResponse = await addBlockAttrAPI(
       {
         "custom-latex-code": inputTextValue,
+      },
+      g_thisWidgetId
+    );
+    let lineNumberResponse = await addBlockAttrAPI(
+      {
+        "custom-line-number": JSON.stringify(lineNumber.checked),
+      },
+      g_thisWidgetId
+    );
+    let blockEndingResponse = await addBlockAttrAPI(
+      {
+        "custom-block-ending": JSON.stringify(blockEnding.checked),
       },
       g_thisWidgetId
     );
@@ -81,15 +103,19 @@ try {
     inputText.style.display = "none";
     modify.style.display = "block";
     lineNumberContainer.style.display = "none";
+    blockEndingContainer.style.display = "none";
     submit.style.display = "none";
     // 渲染伪代码
     pseudocode.renderElement(document.getElementById("output-code"), {
       lineNumber: lineNumber.checked,
+      noEnd: !blockEnding.checked,
     });
   }
 
+  // 提交 点击 监听事件
   submit.addEventListener("click", getCode);
 
+  // 修改 点击 监听事件
   modify.addEventListener("click", async () => {
     // 清空 outputContainer
     outputContainer.innerHTML = "";
@@ -98,10 +124,13 @@ try {
     // 如果有保存的内容，设置到输入框中
     if (savedContent.data["custom-latex-code"]) {  
       inputText.value = savedContent.data["custom-latex-code"];
+      lineNumber.checked = JSON.parse(savedContent.data["custom-line-number"]);
+      blockEnding.checked = JSON.parse(savedContent.data["custom-block-ending"]);
     }
     // 设置按钮显示与隐藏
     inputText.style.display = "block";
     lineNumberContainer.style.display = "block";
+    blockEndingContainer.style.display = "block";
     submit.style.display = "block";
     modify.style.display = "none";
   });
